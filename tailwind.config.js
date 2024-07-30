00a52889334989c13d8441d6721aae022d31b424
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-07-30 11:15:58 +0100

    Reestablishing file links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}", ".src/*.css"],
+  content: ["./*.{html,js}", ".src/*.css"],
   theme: {
     extend: {
       colors: {
